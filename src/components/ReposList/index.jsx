eada44f2fec86301861edbfb50e48b04d1a014ca
marcodev2131 <145700074+marcodev2131@users.noreply.github.com>
2024-07-28 21:21:46 -0300

    criação da mensagem de erro

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -1,31 +1,41 @@
 import { useEffect, useState } from "react";
-
 import styles from './ReposList.module.css';
 
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [erro, setErro] = useState(null);
 
     useEffect(() => {
         setEstaCarregando(true);
+        setErro(null); // Resetar o erro ao iniciar a nova busca
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then(res => res.json())
-        .then(resJson => {
-            setTimeout(() => {
+            .then(res => res.json())
+            .then(resJson => {
+                if (resJson.message) { // Verificar se há uma mensagem de erro
+                    setEstaCarregando(false);
+                    setErro(resJson.message === "Not Found" ? "Perfil não encontrado" : resJson.message);
+                } else {
+                    setTimeout(() => {
+                        setEstaCarregando(false);
+                        setRepos(resJson);
+                    }, 3000);
+                }
+            })
+            .catch(error => {
                 setEstaCarregando(false);
-                setRepos(resJson);
-            }, 3000);
-
-        })
+                setErro('Erro ao carregar repositórios');
+            });
     }, [nomeUsuario]);
 
     return (
         <div className="container">
             {estaCarregando ? (
                 <h1>Carregando...</h1>
+            ) : erro ? (
+                <h1 className={styles.error}>{erro}</h1>
             ) : (
                 <ul className={styles.list}>
-                    {/* {repos.map(repositorio => ( */}
                     {repos.map(({ id, name, language, html_url }) => (
                         <li className={styles.listItem} key={id}>
                             <div className={styles.itemName}>
